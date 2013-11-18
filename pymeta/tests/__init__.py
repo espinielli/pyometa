bde74dc1626b5f8c1db6796d82fad01306776d0d
Enrico Spinielli <enrico.spinielli@gmail.com>
2013-11-18 11:02:38 +0100

    handle removed files

--- pymeta/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-from .test_builder import PythonWriterTests
-from .test_pymeta import (HandyWrapper, MakeGrammarTest, NullOptimizerTest, 
-    OMetaTestCase, PyExtractorTest, SelfHostingTest)
-from .test_runtime import RuntimeTests
